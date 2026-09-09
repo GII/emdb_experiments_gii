16eb288b12088da54b846a286a545a2fc23612d6
efallash <efallashdez@gmail.com>
2026-09-09 18:14:53 +0200

    Properly export bartender_exp files

--- experiments/setup.py
+++ experiments/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
         (os.path.join('share', package_name, 'experiments'), glob('experiments/*.[yma]*')),
+        (os.path.join('share', package_name, 'experiments/bartender_exp'), glob('experiments/bartender_exp/*.[yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
